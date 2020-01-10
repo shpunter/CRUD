b840dbcdd918a0a003604f9dd708d55f9d95fb97
Brudnovskyi Oleksandr <shpunterman@gmail.com>
2020-01-10 19:56:20 +0200

    add unescape

--- frontend/src/store/actions/employees.js
+++ frontend/src/store/actions/employees.js
@@ -1,12 +1,12 @@
 import { HTTP_CREATE_EMPLOYEE, HTTP_PATCH_EMPLOYEE, HTTP_FETCHED_EMPLOYEES, HTTP_DELETE_EMPLOYEE } from "../types"
 
-const unescape = string => string.replace(new RegExp("&"+"#"+"x27;", "g"), "'")
-    .replace(new RegExp("&"+"lt;", "g"), "<")
-    .replace(new RegExp("&"+"gt;", "g"), ">")
-    .replace(new RegExp("&"+"amp;", "g"), "&")
-    .replace(new RegExp("&"+"quot;", "g"), "\"")
-    .replace(new RegExp("&"+"#"+"x5C;", "g"), "\\")
-    .replace(new RegExp("&"+"#"+"x2F;", "g"), "\/")
+const unescape = string => string.replace(new RegExp("&#x27;", "g"), "'")
+    .replace(new RegExp("&lt;", "g"), "<")
+    .replace(new RegExp("&gt;", "g"), ">")
+    .replace(new RegExp("&amp;", "g"), "&")
+    .replace(new RegExp("&quot;", "g"), "\"")
+    .replace(new RegExp("&#x5C;", "g"), "\\")
+    .replace(new RegExp("&#x2F;", "g"), "/")
 
 export const fetchedEmployees = employees => ({
     type: HTTP_FETCHED_EMPLOYEES,
