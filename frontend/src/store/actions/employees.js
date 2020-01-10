5a63b31bb5e47654067fae5d3f62bcd6a74ea222
Brudnovskyi Oleksandr <shpunterman@gmail.com>
2020-01-10 19:51:29 +0200

    add unescape

--- frontend/src/store/actions/employees.js
+++ frontend/src/store/actions/employees.js
@@ -1,8 +1,16 @@
 import { HTTP_CREATE_EMPLOYEE, HTTP_PATCH_EMPLOYEE, HTTP_FETCHED_EMPLOYEES, HTTP_DELETE_EMPLOYEE } from "../types"
 
+const unescape = string => string.replace(new RegExp("&"+"#"+"x27;", "g"), "'")
+    .replace(new RegExp("&"+"lt;", "g"), "<")
+    .replace(new RegExp("&"+"gt;", "g"), ">")
+    .replace(new RegExp("&"+"amp;", "g"), "&")
+    .replace(new RegExp("&"+"quot;", "g"), "\"")
+    .replace(new RegExp("&"+"#"+"x5C;", "g"), "\\")
+    .replace(new RegExp("&"+"#"+"x2F;", "g"), "\/")
+
 export const fetchedEmployees = employees => ({
     type: HTTP_FETCHED_EMPLOYEES,
-    employees: employees.map(({ _id: id, ...rest }) => ({ id, ...rest }))
+    employees: employees.map(({ _id: id, name, ...rest }) => ({ id, name:  unescape(name), ...rest }))
 })
 
 export const deleteEmployee = id => ({
